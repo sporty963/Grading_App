34df09b1a986e16f48422518703490b7dcfd0bfd
GLNRO <lg@glnro.com>
2015-12-02 11:34:58 -0500

    Add giveGrade function

--- gradingapp.js
+++ gradingapp.js
@@ -1,6 +1,15 @@
-// Grading App v1.0
- btnSubmit.addEventListener("click", function(){
+// Grading App
+function giveGrade() {
+  var grade = document.getElementById("grade").value;
 
-    var grade = document.getElementById("grade").value;
+  console.log("we're here!");
+  alert("hey!");
 
 //Input your code below:
+    if(grade == 55){
+      alert("hello");
+    }
+    else{
+      alert("goodbye");
+    }
+  }
