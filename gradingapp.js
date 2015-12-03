b62ca074d09161db934f3f80149a16282ffaad16
sporty963 <ms.harrop963@gmail.com>
2015-12-03 22:10:47 +0000

    hello

--- gradingapp.js
+++ gradingapp.js
@@ -3,8 +3,18 @@ function giveGrade() {
   var grade = document.getElementById("grade").value;
 
   console.log("we're here!");
-  alert("hey!");
-
+  
+if (grade<=100 && grade>=90){
+  alert("A");}
+else if (grade<=89 && grade>=80){
+  alert("B");} 
+else if (grade<=79 && grade>=70){
+  alert("C");} 
+else if (grade<=69 && grade>=65){
+  alert("D");} 
+else if (grade<=64){
+  alert("F");} 
+  
+}
 //Input your code below:
 
-  }
